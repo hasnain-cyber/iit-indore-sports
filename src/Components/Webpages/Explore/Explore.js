f1601011825fdc654991a137e1b9841d08d667a6
Hasnain Raza <hasnainraza1072@gmail.com>
2021-05-12 14:47:28 +0530

    explore page tweaks

--- src/Components/Webpages/Explore/Explore.js
+++ src/Components/Webpages/Explore/Explore.js
@@ -42,9 +42,14 @@ function Explore() {
         return (
             <div id='explorePageReturnWrapper'>
                 <Navbar/>
-                <div id="videoWrapper">
-                    <video src={'/train.mp4'} autoPlay controls muted/>
-                </div>
+                <video src={'/train.mp4'} autoPlay controls muted/>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius eos, fuga fugiat inventore
+                    quasi ullam vero? Ad autem blanditiis commodi consectetur debitis, distinctio, dolorum excepturi
+                    magni non repellendus voluptas?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+                    deleniti dolore esse, eveniet ex excepturi fugit incidunt itaque iusto laboriosam maiores maxime
+                    modi molestiae pariatur quod sapiente sequi suscipit, unde. Lorem ipsum dolor sit amet
+                </p>
                 <div id="cardListReturnWrapper">
                     {cardList}
                 </div>
